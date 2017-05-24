3bfe25c1bb576f0adc89fb29147e1bbf960a49fa
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-05-24 17:01:56 +0300

    Map somewhat correctly over Helsinki

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Map, Marker, Popup, TileLayer, FeatureGroup } from 'react-leaflet';
+
+class OneMap extends Component {
+
+  render() {
+    const position = [60.162059, 24.945831]; // Default to Helsinki's center
+    return (
+      <Map center={position} zoom={14} minZoom={8}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
+}
+
+/*
+<FeatureGroup
+  ref={(input) => {
+    if (!input) return;
+    const bounds = input.leafletElement.getBounds();
+    if (bounds.isValid()) {
+      input.props.map.fitBounds(bounds);
+      const viewportBounds = [
+        [59.9, 24.59],  // SouthWest corner
+        [60.43, 25.3]  // NorthEast corner
+      ];  // Wide Bounds of City of Helsinki area
+      input.props.map.setMaxBounds(viewportBounds);
+    }
+  }}
+>{contents}</FeatureGroup>
+*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="map">
+        <OneMap />
       </div>
     );
   }
